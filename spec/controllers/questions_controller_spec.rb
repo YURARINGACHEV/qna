144f2e0d103076bef1c8eef07d7902fe6c09e437
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-11-07 23:05:15 +1000

    Question POST #create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -58,6 +58,29 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end 
 
+  describe 'POST #create' do 
+    #context задает условия
+    context 'with valid attributes' do 
+      it 'save a new question in the database' do 
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1) #attributes_for(:question)-выдае атрибуты из FactoryBot
+      end
+      it 'redirects to show view' do 
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do 
+      it 'does not save the question' do 
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+      it 're-renders new view' do 
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
   # describe 'GET #index' do 
   #   it 'populates an array of all questions' do 
   #     question1 = FactoryBot.create(:question) #создать вопрос по шаблону factories/questions.rb
