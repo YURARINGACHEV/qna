91bc22e408d22ad69d644c4f6664accde0ed45d0
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-11-22 15:57:53 +1000

    add devise, create scenario 'Registered user tries to sign in'

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,14 +5,17 @@ feature 'Use can sign in', %q{
 	As an unanuthenticated user
 	I`d like to be able to sign in
 } do
+
 	scenario 'Registered user tries to sign in' do 
     User.create!(email: 'user@test.com', password: '12345678')
 
-    visit '/login'
+    visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
+    click_on 'Log in'
 
-    expect(page).to have_content 'Signed in successfuly.'
+    # save_and_open_page открывае страницу в браузуре
+    expect(page).to have_content 'Signed in successfully.'
 	end
 
 	scenario 'Unregistered user tries to sign in' 
