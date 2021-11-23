9935dcb89c6386aa64e60463be80df6dcd9bab94
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-11-23 11:53:19 +1000

    created scenario " Anuthenticated user asks a question"

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Use can create question', %q{
+	In oreder to get answer from community
+	As an anuthenticated user
+	I`d like to be able to ask the question
+} do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') } 
+
+  scenario 'Anuthenticated user asks a question' do
+  	visit visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Text text tex'
+    click_on 'Ask'
+
+
+    expect(page).to have_content "Your question successfully created."
+    expect(page).to have_content "Test question"
+    expect(page).to have_content "Text text tex"
+  end 
+  scenario 'Anuthenticated user asks a question with errors'
+  scenario 'Unanuthenticated user tries to  asks a question'
+end
