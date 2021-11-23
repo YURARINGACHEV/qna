87c11debb64065c05e3f9e3c7bc2283667903011
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-11-23 13:50:32 +1000

    created scenario "Unanuthenticated user tries to  asks a question"

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -26,7 +26,18 @@ feature 'Use can create question', %q{
     expect(page).to have_content "Test question"
     expect(page).to have_content "Text text tex"
   end 
-  scenario 'Anuthenticated user asks a question with errors'
+  scenario 'Anuthenticated user asks a question with errors' do
+  	visit visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+
+    click_on 'Ask'
+    expect(page).to have_content "Title can't be blank"
+  end
 
   scenario 'Unanuthenticated user tries to  asks a question' do
   	visit questions_path
