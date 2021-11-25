54f979728c3b767dc32ee3c897b75bd8f431f775
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-11-25 17:33:37 +1000

    автор может удалять вопрос

---
+++ spec/features/question/delete_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'User can delete own question', %q{
+  In order to delete question
+  As  Authenticated User
+  I`d like to be able to delete own question
+} do
+
+  given(:bad_user) { create(:user) }
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+
+  background { visit question_path(question) }
+
+  scenario 'Authenticated user can delete own questions' do
+    sign_in(user)
+    visit question_path(question)
+
+    click_on "Delete"
+ 
+    expect(page).to have_content "Question deleted"
+    expect(page).to_not have_content question.title
+  end
+
+  scenario 'Authenticated user can delete own questions' do
+    sign_in(bad_user)
+
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to_not have_content "Delete"
+  end
+
+  scenario 'Unauthenticated user can delete own questions' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to_not have_content "Delete"
+  end
+end
