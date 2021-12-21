4b4897e877b8571f5ffa3e4291cb3a29440d71ad
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-12-21 12:21:05 +1000

    add polymorphic linkable

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
-feature 'User can add linkes to question', %q{
+feature 'User can add linkes to answer', %q{
 	In order to provide additional info to my question
 	As an question`s author
 	I`d like to be able to add links
 } do
 	given(:user) { create(:user) }
+  given(:question) { create(:question) }
 	given(:gist_url) { 'https://gist.github.com/YURARINGACHEV/c65a114fac192ae484e5e8dad80cdd5a' }
 
 	scenario 'User add link when asks question' do
     sign_in(user)
-    visit new_question_path
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+    visit question_path(question)
+    
+    fill_in 'Body', with: 'answer answer answer'
 
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
-    click_on 'Ask'
+    click_on 'Answer'
 
-    expect(page).to have_link 'My gist', href: gist_url
+    within '.answers' do
+      expect(page).to have_link 'My gist', href: gist_url
+    end  
   end
 end
