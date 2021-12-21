f2fa04a016f39e650c3898f8952b0e54094fcda6
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-12-21 10:36:55 +1000

    add accepts_nested_attributes_for :links

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,11 @@
 class Question < ApplicationRecord 
   belongs_to :user
   has_many :answers, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links
 
   validates :title, :body, presence: true
 end
