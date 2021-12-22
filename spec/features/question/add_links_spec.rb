8d061034295bb0bdf010f18496e9e2bfb41dce82
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2021-12-22 13:57:36 +1000

    add links for question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -1,25 +1,54 @@
 require 'rails_helper'
 
 feature 'User can add linkes to question', %q{
-	In order to provide additional info to my question
-	As an question`s author
-	I`d like to be able to add links
+  In order to provide additional info to my question
+  As an question`s author
+  I`d like to be able to add links
 } do
-	given(:user) { create(:user) }
-	given(:gist_url) { 'https://gist.github.com/YURARINGACHEV/c65a114fac192ae484e5e8dad80cdd5a' }
+  given(:user) { create(:user) }
+  given(:url_1) { 'https://url_1.com' }
+  given(:url_2) { 'https://url_2.com' }
 
-	scenario 'User add link when asks question' do
-    sign_in(user)
-    visit new_question_path
+  describe 'Add links to neew question'
+    background do
+      sign_in(user)
+      visit new_question_path
+    end
 
+
+  scenario 'User add link when asks question' do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    fill_in 'Link name', with: 'My url 1'
+    fill_in 'Url', with: url_1
 
     click_on 'Ask'
 
-    expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My url 1', href: url_1
+  end
+
+  scenario 'User add many links when asks question', js: true do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    click_on 'add link'
+
+    within page.all('.nested-fields')[0] do
+      fill_in 'Link name', with: 'My url 1'
+      fill_in 'Url', with: url_1
+    end
+
+    click_on 'add link'
+
+    within page.all('.nested-fields')[1] do
+      fill_in 'Link name', with: 'My url 2'
+      fill_in 'Url', with: url_2
+    end
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'My url 1', href: url_1
+    expect(page).to have_link 'My url 2', href: url_2
   end
 end
