e7c5dfeaad7f7530a654606365c3d9cdb82ba6df
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2022-01-11 08:49:47 +1000

    add spec vote for question. add model vote

--- spec/features/question/vote_spec.rb
+++ spec/features/question/vote_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'User can vote for a answer', %q{
-  In order to show that answer is good
+feature 'User can vote for a question', %q{
+  In order to show that question is good
   As an authenticated user
   I'd like to be able to vote
 } do
@@ -9,6 +9,7 @@ feature 'User can vote for a answer', %q{
   given(:author) { create(:user) }
   given(:user) { create(:user) }
   given!(:question) { create(:question, user: author) }
+
 
   describe 'User is not an author of question', js: true do
     background do
@@ -25,5 +26,81 @@ feature 'User can vote for a answer', %q{
         end
       end
     end
+
+    scenario 'you cannot vote multiple times' do
+      within ".question" do
+        click_on 'vote up'
+        click_on 'vote up'
+
+        within '.vote-score' do
+          expect(page).to have_content '1'
+        end
+      end
+    end
+
+    scenario 'unvote his vote' do
+      within ".question" do
+        click_on 'vote up'
+        click_on 'unvote'
+
+        within '.vote-score' do
+          expect(page).to have_content '0'
+        end
+      end
+    end
+
+    scenario 'votes down for question' do
+      within ".question" do
+        click_on 'vote down'
+
+        within '.vote-score' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+
+    scenario 'you can not vote multiple times' do
+      within ".question" do
+        click_on 'vote down'
+        click_on 'vote down'
+
+        within '.vote-score' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+
+    scenario 'can re-votes' do
+      within ".question" do
+        click_on 'vote up'
+        click_on 'unvote'
+        click_on 'vote down'
+
+        within '.vote-score' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
   end
-end
+
+  describe 'User is author of question tries to', js: true do
+    scenario 'Author not have view links' do
+      sign_in(author)
+      visit question_path(question)
+
+      expect(page).to_not have_content 'vote up'
+      expect(page).to_not have_content 'vote down'
+      expect(page).to_not have_content 'unvote'
+    end
+  end
+
+  describe 'Unauthorized user tries to' do
+    scenario 'User not have view links' do
+      visit question_path(question)
+
+      expect(page).to_not have_content 'vote up'
+      expect(page).to_not have_content 'vote down'
+      expect(page).to_not have_content 'unvote'
+    end
+  end
+end 
