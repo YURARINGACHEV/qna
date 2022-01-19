e7c81482a2b495d9e9d2d617f591e1ef08cc8335
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2022-01-20 00:29:47 +1000

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :questions, dependent: :destroy
-  has_many :authorizations
+  has_many :authorizations, dependent: :destroy
 
   has_many :rewards, dependent: :destroy
   has_many :votes, dependent: :destroy
@@ -19,19 +19,7 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    return authorization.user if authorization
-
-    email = auth.info[:email]
-    user = User.where(email: email).first
-    if user
-      user.create_authorization(auth)
-    else
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
-    end
-    user
+    FindForOauthService.new(auth).call
   end
 
   def create_authorization(auth)
