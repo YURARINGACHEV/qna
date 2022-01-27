c1644f1b4aeb6490b9e9c67a5d5145be9fca97cb
YURA RINGACHEV <YOUR EMAIL ADDRESS>
2022-01-27 14:59:44 +1000

    list users

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -15,13 +15,11 @@ describe 'Profiles Api', type: :request do
 
 			before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers }
 
-			it 'renders 200 status' do  		
-		  	expect(response).to be_successful
-		  end
+		 it_behaves_like 'Success requestable'
 
 		  it 'returns all public fields' do
-        %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+        %w[id email admin created_at updated_at].each do |att|
+          expect(json['user'][att]).to eq me.send(att).as_json
         end
 		  end
 
@@ -32,4 +30,39 @@ describe 'Profiles Api', type: :request do
 		  end
 		end 	  
 	end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) {'/api/v1/profiles'}
+    it_behaves_like 'API Authorizable' do
+      let(:method) {:get }
+    end
+
+    context 'authorized' do
+      let(:me) {create(:user)}
+      let(:access_token) { create(:access_token) }
+      let!(:users) {create_list(:user, 2)}
+      let(:user) { users.first }
+      let(:user_response) { json['users'].first }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it_behaves_like 'Success requestable'
+
+      it 'return all public fields' do
+    		%w[id email admin created_at updated_at].each do |attr|
+          expect(user_response[attr]).to eq user.send(attr).as_json
+        end
+      end
+
+      it 'returns list of resources' do
+        expect(json['users'].size).to eq users.size
+      end
+
+      it 'does not returns private fields' do
+      	%w[password encripted_password].each do |attr|
+      		expect(json).to_not have_key(attr)
+      	end
+      end
+    end
+  end
 end
--- spec/support/shared/api_authorization.rb
+++ spec/support/shared/api_authorization.rb
@@ -1,13 +1,19 @@
 shared_examples_for 'API Authorizable' do
   context 'Unauthorized' do
-  		it 'renders401 status if there is access_token' do 
-	  		do_request(method, api_path, headers: headers)
-		  	expect(response.status).to eq 401
-		  end
+  	it 'renders401 status if there is access_token' do 
+	  	do_request(method, api_path, headers: headers)
+		 	expect(response.status).to eq 401
+		 end
 
-		  it 'renders401 status if there is invalid' do 
-			  do_request(method,api_path, params: { access_token: '123' }, headers: headers)
-			  expect(response.status).to eq 401
-		  end
-		end
+		it 'renders401 status if there is invalid' do 
+		  do_request(method,api_path, params: { access_token: '123' }, headers: headers)
+		  expect(response.status).to eq 401
+		 end
+	end
+end
+
+shared_examples_for 'Success requestable' do
+  it 'return success status' do
+    expect(response).to be_successful
+  end
 end
